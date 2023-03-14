bbe1c595cc971f08e848d0f7943f4a1249d7f458
Uriel Traba <trabauriel@gmail.com>
2023-03-13 23:33:17 -0300

    fixing

--- src/components/CartComponent/CartComponent.jsx
+++ src/components/CartComponent/CartComponent.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import cart from './cart.jpg'
 
 export const CartComponent = () => {
   return (
     <div>
-      <div><p style={{position:"fixed",top:"2px"}}>1</p></div>
-      <img src="./CartComponentImg/cart.png" style={{height:"30px",width:"30px"}} alt="" />
+      <div><p style={{position:"fixed",top:"2px",fontSize:"15Apx"}}>1</p></div>
+      <img src={cart} style={{height:"30px",width:"30px"}} alt="" />
       
 
     </div>
--- src/components/NavBarComponent/NavBarComponent.jsx
+++ src/components/NavBarComponent/NavBarComponent.jsx
@@ -3,7 +3,9 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { CartComponent } from '../CartComponent/CartComponent';
+import { CartComponent} from '../CartComponent/CartComponent';
+
+
 
 
 export const NavBarComponent = () => {
